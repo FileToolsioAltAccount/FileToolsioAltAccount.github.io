60311f7b4420ca41125f703ee5715d29cf60df11
FileToolsioAltAccount <153949621+FileToolsioAltAccount@users.noreply.github.com>
2024-02-02 01:21:47 +0000

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Nav from './homepage/jsx/Nav';
 
 export default function App() {
     return (
         <>
-            
+            <Nav />
         </>
     );
 }
