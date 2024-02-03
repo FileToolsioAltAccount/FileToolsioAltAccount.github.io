517e76bc68fb79eb1de8053cd3229646bd81605f
FileToolsioAltAccount <153949621+FileToolsioAltAccount@users.noreply.github.com>
2024-02-03 23:13:13 +0000

    added offline/online

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import { useEffect } from 'react';
 import Nav from './components/jsx/nav';
 
 function App() {
-   return (
-       <>
-           <Nav />
-       </>
-   );
+    useEffect(() => {
+        function checkOnlineStatus() {
+            if (navigator.onLine) {
+                console.log("You are online");
+            } else {
+                console.log("You are offline");
+            }
+        }
+
+        // Initial check
+        checkOnlineStatus();
+
+        // Event listener for online/offline events
+        window.addEventListener('online', checkOnlineStatus);
+        window.addEventListener('offline', checkOnlineStatus);
+
+        // Cleanup: remove event listeners when the component is unmounted
+        return () => {
+            window.removeEventListener('online', checkOnlineStatus);
+            window.removeEventListener('offline', checkOnlineStatus);
+        };
+    }, []);
+
+    return (
+        <>
+            <Nav />
+        </>
+    );
 }
 
 export default App;
