57d25bc75224e2d89ecd449a9bb01bdfe878fb0f
FileToolsioAltAccount <153949621+FileToolsioAltAccount@users.noreply.github.com>
2024-02-03 00:59:29 +0000

    added better readabilty

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,27 @@
-module.exports = {
+const configurationDetails = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { 
+    browser: true, 
+    es2020: true 
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  ignorePatterns: [
+    'dist', '.eslintrc.cjs'
+  ],
+  parserOptions: { 
+    ecmaVersion: 'latest', 
+    sourceType: 'module' 
+  },
+  settings: { 
+    react: { 
+      version: '18.2' 
+    } 
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
@@ -18,3 +30,5 @@ module.exports = {
     ],
   },
 }
+
+module.exports = configurationDetails;
