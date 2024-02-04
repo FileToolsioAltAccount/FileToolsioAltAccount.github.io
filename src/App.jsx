d7eecb1e922ea7b5b88cedbdb743268021fab493
FileToolsioAltAccount <153949621+FileToolsioAltAccount@users.noreply.github.com>
2024-02-04 00:20:59 +0000

    updated load speads

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,10 @@
-import { useEffect, useState } from 'react';
+import React from 'react';
 import Nav from './components/jsx/nav';
 
 function App() {
-    const [online, setOnline] = useState(navigator.onLine);
-
-    useEffect(() => {
-        const checkOnlineStatus = () => {
-            setOnline(navigator.onLine);
-        };
-
-        checkOnlineStatus();
-
-        window.addEventListener('online', checkOnlineStatus);
-        window.addEventListener('offline', checkOnlineStatus);
-
-        return () => {
-            window.removeEventListener('online', checkOnlineStatus);
-            window.removeEventListener('offline', checkOnlineStatus);
-        };
-    }, []);
-
     return (
         <>
-            {online ? (
-                <Nav />
-            ) : (
-                <div>
-                    <p>You are currently offline. Please check your internet connection.</p>
-                </div>
-            )}
+            <Nav />
         </>
     );
 }
