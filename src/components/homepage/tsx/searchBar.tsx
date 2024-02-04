7ee194b35096a739e4f9bc104c3f696ac13f5abe
FileToolsioAltAccount <153949621+FileToolsioAltAccount@users.noreply.github.com>
2024-02-04 23:16:07 +0000

    added props

--- src/components/homepage/tsx/searchBar.tsx
+++ src/components/homepage/tsx/searchBar.tsx
@@ -1,30 +1,30 @@
+import React, { useState, useEffect } from 'react';
 import '../css/searchBar.css';
-import { useState, useEffect } from 'react';
 
-function SearchBar() {
-   const [placeholder, setPlaceholder] = useState('');
+interface SearchBarProps {}
 
-   useEffect(() => {
-       function updatePlaceholder() {
-           if (window.innerWidth < 701) setPlaceholder('Search');
-           else if (window.innerWidth < 1001) setPlaceholder('Search for tools');
-           else setPlaceholder('Search for file tools');
-       }
+const SearchBar: React.FC<SearchBarProps> = () => {
+    const [placeholder, setPlaceholder] = useState<string>('');
 
-       window.addEventListener('resize', updatePlaceholder);
-       updatePlaceholder(); 
-       
-       return () => window.removeEventListener('resize', updatePlaceholder);
-   }, []); 
+    useEffect(() => {
+        const updatePlaceholder = () => {
+            if (window.innerWidth < 701) setPlaceholder('Search');
+            else if (window.innerWidth < 1001) setPlaceholder('Search for tools');
+            else setPlaceholder('Search for file tools');
+        };
 
-   return (
-       <>
-           <div className='search-bar'>
-               <span className='material-symbols-outlined'>search</span>
-               <input className='input' placeholder={placeholder} type='text' />
-           </div>
-       </>
-   );
+        window.addEventListener('resize', updatePlaceholder);
+        updatePlaceholder();
+
+        return () => window.removeEventListener('resize', updatePlaceholder);
+    }, []); 
+
+    return (
+        <div className='search-bar'>
+            <span className='material-symbols-outlined'>search</span>
+            <input className='input' placeholder={placeholder} type='text' />
+        </div>
+    );
 }
 
 export default SearchBar;
