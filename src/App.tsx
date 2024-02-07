fe619b66ce9545811ec6f1e8f99dc19b46b05428
FileToolsioAltAccount <153949621+FileToolsioAltAccount@users.noreply.github.com>
2024-02-07 23:57:22 +0000

    added fallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { Suspense } from 'react';
 import Nav from './components/homepage/tsx/nav';
 
 function App() {
     return (
         <>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Nav />
-            </Suspense>
+            <Nav />
         </>
     );
 }
