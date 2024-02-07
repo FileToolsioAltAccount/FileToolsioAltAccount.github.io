e877db36137ab2a8d4a199c2f898755f20b0bdfb
FileToolsioAltAccount <153949621+FileToolsioAltAccount@users.noreply.github.com>
2024-02-07 22:35:08 +0000

    added feature

--- src/components/homepage/tsx/logo.tsx
+++ src/components/homepage/tsx/logo.tsx
@@ -1,25 +1,27 @@
-import logo from '../../../public/logo.png';
-import '../css/logo.css';
+import logo from '../../../public/logo.png'
+import '../css/logo.css'
 
 function Logo() {
     const redirectToHomepage = () => {
         try {
-            window.location.href = 'https://cuddly-palm-tree-5gg57r7gv5gp37qwv-5173.app.github.dev';
+            window.location.href = 'https://filetoolsioaltaccount.github.io'
         } catch (error) {
-            alert('An error occurred.');
+            alert('An error occurred.')
         }
     }
-    
-    return (
-            <img
-                className='logo'
-                src={logo}
-                width='183.9px'
-                height='27.9px'
-                alt='Logo'
-                onClick={redirectToHomepage}
-            />
-    );
-};
 
-export default Logo;
+    return (
+        <img
+            draggable='false'
+            rel='preload'
+            className='logo'
+            src={logo}
+            width='183.9px'
+            height='27.9px'
+            alt='Logo'
+            onClick={redirectToHomepage}
+        />
+    )
+}
+
+export default Logo
