6a1b60c1d729ea327116066f8df71005a13681f9
FileToolsioAltAccount <153949621+FileToolsioAltAccount@users.noreply.github.com>
2024-02-09 01:25:40 +0000

    added resize

--- src/components/homepage/tsx/logo.tsx
+++ src/components/homepage/tsx/logo.tsx
@@ -17,7 +17,7 @@ const Logo: React.FC = () => {
         const img = new Image();
         img.src = logo;
         img.onload = () => {
-            setSize({ width: img.width / 1.3, height: img.height / 1.3 });
+            setSize({ width: img.width / 1.375, height: img.height / 1.375 });
         };
     }, []);
 
