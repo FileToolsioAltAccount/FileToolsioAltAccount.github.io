5d440bd739dad3173bc6ab8aaa015278215ed09e
FileToolsioAltAccount <153949621+FileToolsioAltAccount@users.noreply.github.com>
2024-02-09 03:29:14 +0000

    added better performance

--- src/components/homepage/tsx/logo.tsx
+++ src/components/homepage/tsx/logo.tsx
@@ -1,49 +1,27 @@
-import React, { useState, useCallback, useEffect, useRef } from 'react';
 import logo from '../../../public/logo.png';
 import '../css/logo.css';
 
-const Logo: React.FC = () => {
-    const [size, setSize] = useState<{ width: number, height: number } | null>(null);
-    const resizeTimeoutId = useRef<number | undefined>();
-
-    const calculateImageSize = useCallback(() => {
-        const img = new Image();
-        img.src = logo;
-        img.onload = () => {
-            setSize({ width: img.width /  1.4, height: img.height /  1.4 });
-        };
-    }, []);
-
-    const handleResize = useCallback(() => {
-        window.clearTimeout(resizeTimeoutId.current);
-        resizeTimeoutId.current = window.setTimeout(calculateImageSize,  500);
-    }, [calculateImageSize]);
-
-    useEffect(() => {
-        calculateImageSize();
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
-    }, [calculateImageSize, handleResize]);
-
-    const redirectToHomepage = useCallback(() => {
+const Logo = () => {
+    const redirectToHomepage = () => {
         try {
             window.location.href = 'https://filetoolsioaltaccount.github.io';
         } catch (error) {
             alert('An error occurred.');
         }
-    }, []);
+    };
 
-    if (!size) {
-        return null;
-    }
+    const originalWidth = 262;
+    const originalHeight = 40;
 
     return (
         <img
             className='logo'
             src={logo}
             alt='Logo'
-            width={size.width}
-            height={size.height}
+            style={{
+                width: `${originalWidth /   1.4}px`,
+                height: `${originalHeight /   1.4}px`
+            }}
             onClick={redirectToHomepage}
         />
     );
