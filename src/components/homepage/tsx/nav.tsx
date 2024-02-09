59a1481127c730d5b3cd1bda6db5c32e3ce54b0c
FileToolsioAltAccount <153949621+FileToolsioAltAccount@users.noreply.github.com>
2024-02-09 01:35:24 +0000

    added lazy loading

--- src/components/homepage/tsx/nav.tsx
+++ src/components/homepage/tsx/nav.tsx
@@ -1,10 +1,11 @@
-import { Suspense } from 'react';
+import { Suspense, lazy } from 'react';
 import Advertise from './advertise.tsx';
 import Pricing from './pricing.tsx';
-import Logo from './logo.tsx';
 import SearchBar from './searchBar.tsx';
 import Login from './login.tsx';
 import '../css/nav.css';
+
+const Logo = lazy(() => import('./logo.tsx'));
 
 function Nav() {
         return (
